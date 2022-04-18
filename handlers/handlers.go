0bba6ee41e4c8b8e278b405379c394da74cf846c
the-tunnster <therealtarunkrishnan@gmail.com>
2022-04-18 12:07:09 +0000

    Added two functions for cleaning up the test flow.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,48 +2,46 @@ package handlers
 
 import (
 	"crocStuff/models"
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func ClientPushing(w http.ResponseWriter, r *http.Request) {
 	log.Println("Hit ClientPushing Post Endpoint.")
 
-	var req models.ClientPushingRequest
+	var clientInformation models.Request
 
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := json.NewDecoder(r.Body).Decode(&clientInformation)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	res := models.ClientPushingResponse{Status: "success"}
+	response := models.Response{Status: "success"}
 
 	crocExecutable, _ := exec.LookPath("croc")
-	cmdGoVer := &exec.Cmd{
+	cmdGoCroc := &exec.Cmd{
 		Path:   crocExecutable,
-		Args:   []string{crocExecutable, req.CrocCodePhrase},
+		Args:   []string{crocExecutable, clientInformation.CrocCodePhrase},
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
 
-	json.NewEncoder(w).Encode(res)
+	json.NewEncoder(w).Encode(response)
 
-	err = cmdGoVer.Run()
-
-	if err != nil {
-		log.Println("Error running command")
-	}
-
+	defer Execute(cmdGoCroc)
 }
 
 func ClientPulling(w http.ResponseWriter, r *http.Request) {
 	log.Println("Hit ClientPulling Post Endpoint.")
 
-	var req models.ClientPullingRequest
+	var req models.Request
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -51,10 +49,10 @@ func ClientPulling(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := models.ClientPullingResponse{CrocCodePhrase: "wack-abce"}
+	res := models.Response{CrocCodePhrase: "wack-abce"}
 
 	crocExecutable, _ := exec.LookPath("croc")
-	cmdGoVer := &exec.Cmd{
+	cmdGoCroc := &exec.Cmd{
 		Path:   crocExecutable,
 		Args:   []string{crocExecutable, "send", "--code", "wack-abce", "test.txt"},
 		Stdout: os.Stdout,
@@ -63,14 +61,91 @@ func ClientPulling(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(res)
 
-	err = cmdGoVer.Run()
+	defer Execute(cmdGoCroc)
+}
+
+func Execute(cmdGoCroc *exec.Cmd) {
+	err := cmdGoCroc.Run()
 
 	if err != nil {
 		log.Println("Error running command")
 	}
+}
+
+//The function should accept a request and response writer.
+func TestFlow(w http.ResponseWriter, r *http.Request) {
+
+	//logging statement to verify that the endpoint is being hit.
+	log.Println("Testing endpoint hit.")
+
+	//There needs to be a struct to store the decoded request body in.
+	var clientInfo models.Request
+
+	//A response needs to be instantiated with a default message.
+	var response models.Response
+
+	//The request needs to be decoded, and a response needs to be instantiated.
+	err := json.NewDecoder(r.Body).Decode(&clientInfo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//The struct is passed to the verify function that compares the passed values against the database values.
+	isValid := Verify(clientInfo)
+
+	//If the data is valid, return a response saying so
+	if isValid {
+		response.Status = "Valid"
+	} else {
+		response.Status = "Invalid"
+	}
+
+	json.NewEncoder(w).Encode(response)
 
 }
 
-func API_Endpoint_Test(w http.ResponseWriter, r *http.Request){
-	log.Println("Hit the testing endpoint.")
+//Verify pulls information from a database and then returns true if the fields match up as expected.
+//Can be modified to return a specific error if you wish to know exactly what is wrong.
+func Verify(information models.Request) (valid bool) {
+
+	db, err := sql.Open("mysql", "root:Th!3f3ry@tcp(127.0.0.1:3306)/crocTesting")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("SELECT SC_API_Key, O_API_Key, CO_API_Key FROM userInfo")
+	if err != nil {
+		log.Println(err)
+	}
+
+	for results.Next() {
+		var userInfo models.UserInfo
+
+		err = results.Scan(&userInfo.ServerClientAPIKey, &userInfo.OragnizationAPIKey, &userInfo.ClientOrganizationKey)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if(information.ClientOrganizationKey != userInfo.ClientOrganizationKey){
+			log.Println("CO_API_KEY Mismatch")
+			break
+		}
+		if(information.ServerClientAPIKey != userInfo.ServerClientAPIKey){
+			log.Println("SC_API_KEY Mismatch")
+			break
+		}
+		if(information.OragnizationAPIKey != userInfo.OragnizationAPIKey){
+			log.Println("CO_API_KEY Mismatch")
+			break
+		}
+
+		valid=true
+	}
+
+	return valid
 }
